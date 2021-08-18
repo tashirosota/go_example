121e2097df1f7759433c897d74c434f4f078a89e
Tashiro <g4160hc@gmail.com>
2021-08-18 13:35:49 +0900

    refacter to root.go

--- app/chi.go
+++ app/chi.go
@@ -1,18 +1,17 @@
 package chi
 
 import (
+	"fmt"
 	"net/http"
-	// "context"
-	// "time"
+	"gigphil/app/requests"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
-
+const port string = "4000"
 func Setup(){
+	fmt.Printf("\n===== Listening on http://127.0.0.1:" + port + " =====\n\n")
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
-	http.ListenAndServe(":4000", r)
+	r.Get("/", root.Get)
+	http.ListenAndServe(":" + port, r)
 }
---
+++ app/requests/root.go
@@ -0,0 +1,9 @@
+package root
+
+import (
+	"net/http"
+)
+
+func Get(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("welcome"))
+}
