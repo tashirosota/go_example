640e9dc2bb72e1fa019b788cd07da72484a86ea7
Tashiro <g4160hc@gmail.com>
2021-08-18 16:51:19 +0900

    add procfile

--- app/chi.go
+++ app/chi.go
@@ -5,14 +5,15 @@ import (
 	"gigphil/app/controllers/area"
 	"gigphil/app/controllers/root" // request増えるたびに都度増やすの苦痛だな memo: packageは1dirに付き１つ
 	"net/http"
-
+  "os"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
-
-const port string = "4000"
-
 func Setup() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port =  "4000"
+	}
 	fmt.Printf("\n===== Listening on http://127.0.0.1:" + port + " =====\n\n")
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
